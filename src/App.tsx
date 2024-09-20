6483c865ddfb59b9597eff17d71a8dcaddf1eb55
idavidcastro <89281605+idavidcastro@users.noreply.github.com>
2024-09-20 12:29:47 -0500

    changes

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import RoutesApp from "./app/routes/RoutesApp";
-
-const App = () => {
-  return <RoutesApp />;
-};
-
-export default App;
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,5 @@
-import { CircularProgress, Input } from "@nextui-org/react"
+import RoutesApp from "./routes/RoutesApp";
 
 export const App = () => {
-  return (
-    <>
-      <Input
-        type="email"
-        label="Email"
-        defaultValue="junior@nextui.org"
-        className="max-w-xs"
-      />
-      <CircularProgress aria-label="Loading..." />
-    </>
-  )
-}
+  return <RoutesApp />;
+};
